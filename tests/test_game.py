efa3bd3e2d17805f73c6e3599a94d8049e3c8f23
Tanay <tanayvenkata@gmail.com>
2025-03-16 15:07:35 -0400

    added tests

---
+++ tests/test_game.py
@@ -0,0 +1,22 @@
+import unittest
+from number_guessing_game.game import NumberGame
+
+
+class TestNumberGame(unittest.TestCase):
+    def setUp(self):
+        # Set a fixed random number for testing
+        self.game = NumberGame()
+        self.game.num = 50
+
+    def test_check_guess_higher(self):
+        self.assertEqual(self.game.check_guess(25), "higher")
+
+    def test_check_guess_lower(self):
+        self.assertEqual(self.game.check_guess(75), "lower")
+
+    def test_check_guess_correct(self):
+        self.assertEqual(self.game.check_guess(50), "correct")
+
+
+if __name__ == "__main__":
+    unittest.main()
